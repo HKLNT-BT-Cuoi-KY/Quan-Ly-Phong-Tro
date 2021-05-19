65b05b0ba7059bcf69b3daf11b0017c689c07036
Nga2301 <DELL@192.168.1.24>
2021-05-19 16:34:53 +0700

    Them form quan li nguoi dung

--- quanLyPT/src/javaswing/view/Quanlinguoidung.java
+++ quanLyPT/src/javaswing/view/Quanlinguoidung.java
@@ -9,12 +9,12 @@ package javaswing.view;
  *
  * @author DELL
  */
-public class Quanlinguoithue extends javax.swing.JFrame {
+public class Quanlinguoidung extends javax.swing.JFrame {
 
     /**
      * Creates new form Quanlinguoithue
      */
-    public Quanlinguoithue() {
+    public Quanlinguoidung() {
         initComponents();
     }
 
@@ -298,21 +298,23 @@ public class Quanlinguoithue extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Quanlinguoithue.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Quanlinguoidung.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Quanlinguoithue.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Quanlinguoidung.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Quanlinguoithue.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Quanlinguoidung.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Quanlinguoithue.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Quanlinguoidung.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Quanlinguoithue().setVisible(true);
+                new Quanlinguoidung().setVisible(true);
             }
         });
     }
