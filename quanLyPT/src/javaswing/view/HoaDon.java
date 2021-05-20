560089518146118f501bf3f229bc4304de092970
Tran Khanh <Tran Khanh@DESKTOP-3H4LPE0>
2021-05-20 20:51:52 +0700

    sua hoadon

--- quanLyPT/src/javaswing/view/HoaDon.java
+++ quanLyPT/src/javaswing/view/HoaDon.java
@@ -68,6 +68,11 @@ public class HoaDon extends javax.swing.JFrame {
 
         jButton1.setBackground(new java.awt.Color(0, 153, 153));
         jButton1.setText("Thêm");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setBackground(new java.awt.Color(0, 153, 153));
         jButton2.setText("Xoá");
@@ -132,6 +137,10 @@ public class HoaDon extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        new Them_HoaDon().setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
