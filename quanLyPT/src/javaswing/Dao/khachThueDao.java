a3d73807a06a1b12d015262d69c4850d37f18077
Nga2301 <DELL@192.168.1.24>
2021-05-25 09:39:29 +0700

    viet code cho class khach thue va khachthueDao

--- quanLyPT/src/javaswing/Dao/khachThueDao.java
+++ quanLyPT/src/javaswing/Dao/khachThueDao.java
@@ -5,10 +5,59 @@
  */
 package javaswing.Dao;
 
-/**
- *
- * @author HP
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javaswing.Model.KhachThue;
+
 public class khachThueDao {
-    
+     static Connection con = ConnectDB.getConnectDB();
+    public boolean insert(KhachThue nhan) throws Exception
+    {
+        String sql = "insert into KhachThue(maKH,HoTen,NgaySinh,NgheNghiep,GioiTinh,Sdt,QueQuan,maPH) values(?,?,?,?,?,?,?,?)"; 
+        try(
+            PreparedStatement    pstmt = con.prepareStatement(sql); 
+                ){
+            pstmt.setString(1, nhan.getMaKT());
+            pstmt.setString(2, nhan.getHoTen());
+            pstmt.setString(3, nhan.getNgaySinh());
+            pstmt.setString(4, nhan.getNgheNghiep());
+            pstmt.setString(5, nhan.getGioiTinh());
+            pstmt.setString(6, nhan.getSdt());
+            pstmt.setString(7, nhan.getQueQuan());
+            pstmt.setString(8, nhan.getMaPH());
+            return pstmt.executeUpdate()> 0;
+        }               
+             
+}     
+    public List<KhachThue> getAllUsersKT(){
+        List<KhachThue> kts = new ArrayList<KhachThue>();
+        String sql  = "select * from KhachThue";
+        try { 
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                KhachThue kt  = new KhachThue();
+                
+                kt.setMaKT(rs.getString("MaKT"));
+                kt.setHoTen(rs.getString("TenNV"));
+                kt.setNgaySinh(rs.getString("NgaySinh"));
+                kt.setNgheNghiep(rs.getString("NgheNghiep"));
+                kt.setGioiTinh(rs.getString("GioiTinh"));
+                kt.setSdt(rs.getString("Sdt"));
+                kt.setQueQuan(rs.getString("QueQuan"));
+                kt.setMaPH(rs.getString("maPH"));
+                kts.add(kt);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(khachThueDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return kts;
+    }
 }
--- quanLyPT/src/javaswing/Model/KhachThue.java
+++ quanLyPT/src/javaswing/Model/KhachThue.java
@@ -4,11 +4,85 @@
  * and open the template in the editor.
  */
 package javaswing.Model;
-
-/**
- *
- * @author HP
- */
 public class KhachThue {
+    String maKT,HoTen,NgaySinh,NgheNghiep,GioiTinh,Sdt,QueQuan,maPH;
+
+    public KhachThue() {
+    }
+    
+    public KhachThue(String maKT, String HoTen, String NgaySinh, String NgheNghiep, String GioiTinh, String Sdt, String QueQuan, String maPH) {
+        this.maKT = maKT;
+        this.HoTen = HoTen;
+        this.NgaySinh = NgaySinh;
+        this.NgheNghiep = NgheNghiep;
+        this.GioiTinh = GioiTinh;
+        this.Sdt = Sdt;
+        this.QueQuan = QueQuan;
+        this.maPH = maPH;
+    }
+
+    public String getMaKT() {
+        return maKT;
+    }
+
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    public String getNgaySinh() {
+        return NgaySinh;
+    }
+
+    public String getNgheNghiep() {
+        return NgheNghiep;
+    }
+
+    public String getGioiTinh() {
+        return GioiTinh;
+    }
+
+    public String getSdt() {
+        return Sdt;
+    }
+
+    public String getQueQuan() {
+        return QueQuan;
+    }
+
+    public String getMaPH() {
+        return maPH;
+    }
+
+    public void setMaKT(String maKT) {
+        this.maKT = maKT;
+    }
+
+    public void setHoTen(String HoTen) {
+        this.HoTen = HoTen;
+    }
+
+    public void setNgaySinh(String NgaySinh) {
+        this.NgaySinh = NgaySinh;
+    }
+
+    public void setNgheNghiep(String NgheNghiep) {
+        this.NgheNghiep = NgheNghiep;
+    }
+
+    public void setGioiTinh(String GioiTinh) {
+        this.GioiTinh = GioiTinh;
+    }
+
+    public void setSdt(String Sdt) {
+        this.Sdt = Sdt;
+    }
+
+    public void setQueQuan(String QueQuan) {
+        this.QueQuan = QueQuan;
+    }
+
+    public void setMaPH(String maPH) {
+        this.maPH = maPH;
+    }
     
 }
