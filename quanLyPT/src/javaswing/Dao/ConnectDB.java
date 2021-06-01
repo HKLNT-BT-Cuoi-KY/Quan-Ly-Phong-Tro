43b5fe3562354ddfd5a2fbe120744f96b1abec4f
HoaNguyen <60832263+hoavannguyen682001@users.noreply.github.com>
2021-06-01 20:32:35 +0700

    commit connect

--- quanLyPT/src/javaswing/Dao/ConnectDB.java
+++ quanLyPT/src/javaswing/Dao/ConnectDB.java
@@ -13,11 +13,8 @@ public class ConnectDB {
 
     public static Connection getConnectDB() {
         try {
-//            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-//            String dbUrl = "jdbc:jtds:sqlserver://"
-//                    + Name_PC
-//                    + "/QlyNhaTro";
-            String dbUrl = "jdbc:jtds:sqlserver://DESKTOP-3H4LPE0/QlyNhaTro";
+              Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            String dbUrl = "jdbc:jtds:sqlserver://localhost:1433/QlyNhaTro";
             con = DriverManager.getConnection(dbUrl);
         } catch (Exception e) {
 //            e.printStackTrace();
